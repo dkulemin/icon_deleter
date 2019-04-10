1e211f4feebc4e080653a432da0290b9ffed10ff
dkulemin <blablamaildk@gmail.com>
2019-04-10 11:10:11 +0500

    adding C# code

---
+++ icon_deleter_c_sharp/icon_deleter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace icon_deleter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.Delete("C:\\Users\\d.v.kulemin.UTMN\\Desktop\\Портал Vmeste.url");
+            File.Delete("C:\\Users\\Public\\Desktop\\Техподдержка ТюмГУ.lnk");
+            File.Delete("C:\\Users\\d.v.kulemin.UTMN\\Desktop\\Консультант Плюс.lnk");
+        }
+    }
+}
